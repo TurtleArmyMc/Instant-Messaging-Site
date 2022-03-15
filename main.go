9272d9fc7d494a60779c296babbed16678b22df0
TurtleArmy <44322335+TurtleArmyMc@users.noreply.github.com>
2022-03-15 15:54:34 -0400

    Initial commit.

---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.LoadHTMLGlob("templates/*")
+
+	r.StaticFile("/", "static/html/homepage.html")
+	r.StaticFile("/javascript/room.js", "static/javascript/room.js")
+
+	r.GET("/ping", pong)
+
+	r.GET("/rooms/", roomsGET) // Can have roomname in query
+	r.GET("/room/:roomname", roomGET)
+	r.POST("/room/:roomname", roomPOST)
+	r.GET("/room/:roomname/stream", roomStream)
+
+	r.Run("0.0.0.0:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func pong(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func roomsGET(c *gin.Context) {
+	if roomname := c.Request.URL.Query().Get("roomname"); roomname != "" {
+		c.Redirect(http.StatusTemporaryRedirect, "/room/"+roomname)
+	} else {
+		c.HTML(http.StatusOK, "room_index.tmpl.html", gin.H{"Rooms": roomManager.GetRooms()})
+	}
+}
+
+func roomGET(c *gin.Context) {
+	roomName := c.Param("roomname")
+
+	c.HTML(http.StatusOK, "room.tmpl.html", gin.H{
+		"RoomName": roomName,
+		"Messages": roomManager.GetRoomMessages(roomName),
+	})
+}
+
+// TODO: figure out where to put this
+type MessagePostRequest struct {
+	Content string `json:"message_content" form:"message_content" binding:"required"`
+}
+
+func roomPOST(c *gin.Context) {
+	roomName := c.Param("roomname")
+
+	var request MessagePostRequest
+	if err := c.Bind(&request); err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			// "status":  "success",
+			// "message": request.Message,
+		})
+		return
+	}
+
+	roomManager.PostInRoom(roomName, request.Content)
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  "success",
+		"message": request.Content,
+	})
+}
+
+func roomStream(c *gin.Context) {
+	roomName := c.Param("roomname")
+
+	roomListener := roomManager.AddUserToRoom(roomName)
+	defer roomManager.RemoveUserFromRoom(roomName, roomListener)
+
+	clientGone := c.Writer.CloseNotify()
+	c.Stream(func(w io.Writer) bool {
+		select {
+		case <-clientGone:
+			return false
+		case message := <-roomListener:
+			c.SSEvent("text_message", message)
+			return true
+		}
+	})
+}
---
+++ static/javascript/room.js
@@ -0,0 +1,53 @@
+window.onload = function () {
+    let newMessageForm = document.getElementById("newMessageForm");
+    newMessageForm.focus();
+    newMessageForm.scrollIntoView();
+
+    // Handler func for submitting new messages
+    newMessageForm.onsubmit = function (e) {
+        e.preventDefault();
+        /** @type {HTMLFormElement} */
+        let form = e.target;
+        let data = new FormData(form);
+        fetch(form.action, {
+            method: form.method,
+            body: data,
+        })
+            .catch(function (err) {
+                //Failure
+                alert("Error " + err);
+            });
+        // Clear field after sending a message
+        document.getElementById("messageInput").value = "";
+    };
+
+    if (!!window.EventSource) {
+        // Just using "/stream" as a relative path seems to work fine in
+        // Firefox, but not in Chrome
+        let streamUrl = new URL(document.URL +
+            (document.URL.charAt(document.URL.length-1) === '/' ? "stream" : "/stream")
+        );
+        // Stream of new messages
+        var source = new EventSource(streamUrl);
+        source.addEventListener(
+            "text_message",
+            function (e) {
+                let p = document.createElement("p");
+                p.textContent = e.data;
+                let messages = document.getElementById("messages");
+                messages.appendChild(p);
+                newMessageForm.scrollIntoView();
+            },
+            false
+        );
+
+        // Required in Chrome to ensure that the stream is closed when the
+        // page is
+        window.onbeforeunload = function (e) {
+            source.close();
+        }
+
+    } else {
+        alert("NOT SUPPORTED");
+    }
+};
