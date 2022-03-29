d6448e25f65eeff614d67053d7bffa2e85bacb70
TurtleArmy <44322335+TurtleArmyMc@users.noreply.github.com>
2022-03-29 16:33:33 -0400

    feature: Display timestamps for messages

--- static/javascript/room.js
+++ static/javascript/room.js
@@ -7,13 +7,42 @@ getRelativeUrl = function(url) {
     return new URL(document.URL + '/' + url);
 }
 
+// TODO?: Update timestamps at midnight?
+formatDate = function(date) {
+    let now = new Date();
+    if (date.getDate() == now.getDate() && date.getMonth() == now.getMonth() && date.getYear() == now.getYear()) {
+        // Return time without seconds for messages sent today
+        let match = /(\d{1,2}:\d{2}):\d{2}(.*)/.exec(date.toLocaleTimeString());
+        return match[1] + match[2];
+    } else {
+        // Return date for messages sent on a different day
+        return date.toLocaleDateString();
+    }
+}
+
 displayMessage = function(message) {
-    let p = document.createElement("p");
-    p.classList.add('message');
-    p.textContent = message.content;
+    let messageDiv = document.createElement("div");
+    messageDiv.classList.add("message");
+
+    // timestampDiv wrapped in order to center timestamp vertically
+    let timestampWrapper = document.createElement("div");
+    timestampWrapper.classList.add("timestamp")
+    let timestampDiv = document.createElement("div");
+    let time = new Date(message.time);
+    timestampDiv.textContent = formatDate(time);
+    // TODO: Replace with toggletip
+    // Show full date on hover
+    timestampDiv.setAttribute("title", time.toLocaleString());
+    timestampWrapper.appendChild(timestampDiv);
+    messageDiv.appendChild(timestampWrapper);
+
+    let contentDiv = document.createElement("div");
+    contentDiv.textContent = message.content;
+    messageDiv.appendChild(contentDiv);
+
     let messagesDisplay = document.getElementById("messages");
-    messagesDisplay.insertBefore(p, messagesDisplay.firstChild);
-    p.scrollIntoView();
+    messagesDisplay.insertBefore(messageDiv, messagesDisplay.firstChild);
+    messageDiv.scrollIntoView();
 }
 
 window.onload = function () {
